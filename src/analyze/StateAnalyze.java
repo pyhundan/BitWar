5ca2ef88e91167899925a6e2e0a14bcaaf3093b2
pyhundan <806700892@qq.com>
2019-06-13 23:38:06 +0800

    词法分析第二次完善，初步完成语法分析器

--- src/analyze/StateAnalyze.java
+++ src/analyze/StateAnalyze.java
@@ -3,6 +3,7 @@ package analyze;
 import com.sun.tools.javac.jvm.Code;
 import error.CodeException;
 
+import javax.swing.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -16,7 +17,7 @@ import java.util.LinkedList;
     if_stmt -> if(exp)then '{' stmt_seq'}' {else '{'stmt_seq'}'} end
     repeat_stmt -> repeat(exp) '{'stmt_seq'}' end repeat
     assign_stmt -> identifier:=exp
-    back_stmt -> back(identifier|number) //好像是返回语句
+    back_stmt -> back(identifier|number) //返回语句
     exp -> simple_exp {comparison_op simple_exp}
     comparison_op -> <|=|>
     simple_exp -> term { addop term}
@@ -39,9 +40,8 @@ public class StateAnalyze {
     public HashMap<String,Integer> identifier;
     public int back;
     public boolean backflag;
-    //public BattleHistory battleHistory
+  //  public BattleHistory battleHistory
     //public int oppenent;
-
     //public void setBattleHistory(){};
 
     //构建标识符列表
@@ -117,7 +117,6 @@ public class StateAnalyze {
             }
         }
         else Error("stmt_seq not corresponding! (index :"+(index-1)+",line :"+tok.line+")");
-
     }
 
     public void statement() throws CodeException
@@ -148,63 +147,217 @@ public class StateAnalyze {
     {
         match("if");
         match("(");
+        int flag=exp();
+        match(")");
+        match("{");
+        if (flag==1)
+        {
+            stmt_seq();
+        }
+        else
+        {
+            Token t=tok;
+            int i=index;
+            while(t.word.equals("}"))
+            {
+                if (t.word.equals("{")) {
+                    //match("{");
+                }
+                t=tokens.get(i++);
+            }
+            tok=t;
+            index=i;
+        }
+        match("}");
+        if (tok.word.equals("else")){
+            match("else");
+            match("{");
+            if (flag==1)
+            {
+                index=findright(index-1);
+                tok=tokens.get(index++);
+            }
+            else{
+                stmt_seq();
+            }
+            match("}");
+        }
+        match("end");
     }
 
-    public void repeat_stmt()
+    public void repeat_stmt() throws CodeException
+    {
+        match("for");
+        match("(");
+        int exp_index=index-1;
+        int temp=exp();
+        match(")");
+        match("{");
+        int spin_index=index-1;
+        int spined_index=findright(index-1);
+        while (temp==1){
+            stmt_seq();
+            index=exp_index;
+            tok=tokens.get(index++);
+            temp=exp();
+            index=spin_index;
+            tok=tokens.get(index++);
+        }
+        index=spined_index;
+        tok=tokens.get(index++);
+        match("}");
+        match("end");
+        match("for");
+    }
+
+    public void assign_stmt() throws CodeException
+    {
+        String temp=tok.word;
+        match("identifier");
+        match(":");
+        match("=");
+        int temp1=simple_exp();
+        identifier.put(temp,temp1);
+    }
+
+    public void back_stmt() throws CodeException
+    {
+        match("back");
+        if (tok.type.equals("identifier"))
+        {
+            if (backflag)
+            {
+                back=identifier.get(tok.word);
+                backflag=false;
+            }
+            match("identifier");
+        }
+        else if (tok.type.equals("number"))
+        {
+            if (backflag)
+            {
+                back=Integer.parseInt(tok.word);
+                backflag=false;
+            }
+            match("number");
+        }
+        else Error("back not corresponding! (index :"+(index-1)+",line :"+tok.line+")");
+
+    }
+
+    public int exp() throws CodeException
+    {
+        int temp=simple_exp();
+        while(tok.word.equals("=")||tok.word.equals("<")||tok.word.equals(">"))
+        {
+            char compare=tok.word.charAt(0);
+            match("character");
+            int temp1=simple_exp();
+            switch (compare)
+            {
+                case '=':
+                    if(temp==temp1) temp=1;
+                    else temp=0;
+                    break;
+                case '>':
+                    if (temp>temp1) temp=1;
+                    else temp=0;
+                    break;
+                case '<':
+                    if (temp<temp1) temp=1;
+                    else temp=0;
+                    break;
+            }
+        }
+        return temp;
+    }
+
+    public int simple_exp() throws CodeException
+    {
+        int temp=term();
+        while(tok.word.equals("-")||tok.word.equals("+"))
+        {
+            char c=tok.word.charAt(0);
+            switch (c)
+            {
+                case '-':temp=temp-term();break;
+                case '+':temp=temp+term();break;
+            }
+        }
+        return temp;
+
+    }
+
+
+    public int term() throws CodeException
+    {
+
+        int temp=term();
+        while(tok.word.equals("*")||tok.word.equals("/"))
+        {
+            char c=tok.word.charAt(0);
+            switch (c)
+            {
+                case '*':temp=temp*term();break;
+                case '?':temp=temp/term();break;
+            }
+        }
+        return temp;
+    }
+
+
+
+    public int factor() throws CodeException
+    {
+        int temp=0;
+        if (tok.word.equals("("))
+        {
+            match("(");
+            temp=simple_exp();
+            match(")");
+        }
+        else if (tok.word.equals("number"))
+        {
+            temp=Integer.parseInt(tok.word);
+            match("number");
+        }
+        else if (tok.word.equals("identifier"))
+        {
+            temp=identifier.get(tok.word);
+            match("identifier");
+        }
+        else if (tok.type.equals("system")){
+            if (tok.word.equals("RANDOM")){
+                temp=random();
+            }
+            else if (tok.word.equals("LATEST")){
+
+                match("LATEST");
+            }
+        }
+        else Error("not a factor! (index:"+(index-1)+",line:"+tok.line+")");
+        return temp;
+    }
+
+    public void HISTORY_CALL() throws CodeException
     {
 
     }
 
-    public void assign_stmt()
+    private int findright(int index)
     {
-
+        int temp=index;
+        Token t=tokens.get(index);
+        while(!t.word.equals("}"))
+        {
+            if (t.word.equals("{"))
+            {
+                temp=findright(temp+1);
+            }
+            t=tokens.get(++temp);
+        }
+        return temp;
     }
-
-    public void back_stmt()
-    {
-
-    }
-
-    public void exp()
-    {
-
-    }
-
-    public void comparison_op()
-    {
-
-    }
-
-    public void simple_exp()
-    {
-
-    }
-
-    public void addop()
-    {
-
-    }
-
-    public void term()
-    {
-
-    }
-
-    public void mulop()
-    {
-
-    }
-
-    public void factor()
-    {
-
-    }
-
-    public void HISTORY_CALL()
-    {
-
-    }
-
 
 
 }
